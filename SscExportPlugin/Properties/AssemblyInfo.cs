3ea3ecb010ccd4252444bf506fa646861446f2e8
Paralleltree <paralleltree@outlook.com>
2019-09-15 00:33:38 +0900

    SscExportPluginプロジェクト追加

---
+++ SscExportPlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SscExportPlugin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("paltee.net")]
+[assembly: AssemblyProduct("SscExportPlugin")]
+[assembly: AssemblyCopyright("Copyright (C) 2019 Paralleltree")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("6cca7464-3389-49ac-b2bf-7bbe4191277b")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
